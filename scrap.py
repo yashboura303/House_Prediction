dfd448bcafaed64d77de5f24f6bd7a8c2ae05dcd
yash <yashboura303@gmail.com>
2020-03-08 18:24:47 +0530

    added csv writing for required features

--- scrap.py
+++ scrap.py
@@ -2,19 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 l = []
-for i in range(30):
-	source = requests.get(f'https://www.magicbricks.com/flats-in-mumbai-for-sale-pppfs/page-{i}').text
-	soup = BeautifulSoup(source, 'lxml')
-	for div in soup.find_all("div", class_='flex relative clearfix m-srp-card__container'):
-		l.append(div)
-		print("Price", end=" ")
-		if div.find("span", class_="m-srp-card__price"):
-			print(div.find("span", class_="m-srp-card__price").text)
-		else:
-			print("Call for price")
-		for div in (div.find_all("div", class_="m-srp-card__summary__item"))[:4]:
-			print((div.find("div", class_="m-srp-card__summary__title").text), end=" ")
-			print(div.find("div", class_="m-srp-card__summary__info").text)
-	print("*"*50)
-	print(i)
-print("Count:", len(l))
+with open('data_set.csv', 'w', newline='') as csvfile:
+	fieldnames = ['super_area', 'carpet_area', 'bedrooms', 'bathrooms', 'car_parking', 'furnishing', 'transaction_type','floor', 'facing', 'overlooking', 'price']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+	for i in range(10):
+		source = requests.get(f'https://www.magicbricks.com/flats-in-mumbai-for-sale-pppfs/page-{i}').text
+		soup = BeautifulSoup(source, 'lxml')
+		for div in soup.find_all("div", class_='flex relative clearfix m-srp-card__container'):
+			dic ={}
+			if div.find("span", class_="m-srp-card__price"):
+				dic['price']=div.find("span", class_="m-srp-card__price").text
+			else:
+				print("Call for price")
+			if div.find("span", class_="m-srp-card__title__bhk"):
+				dic['bedrooms']=div.find("span", class_="m-srp-card__title__bhk").text
+			for div in (div.find_all("div", class_="m-srp-card__summary__item")):
+				if div.find("div", class_="m-srp-card__summary__title").text == "society":
+					continue
+				if div.find("div", class_="m-srp-card__summary__title").text == "bathroom":
+					dic['bathrooms'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "furnishing":
+					dic['furnishing'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "transaction":
+					dic['transaction_type'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "super area":
+					dic['super_area'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "carpet area":
+					dic['carpet_area'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "floor":
+					dic['floor'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "facing":
+					dic['facing'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "overlooking":
+					dic['overlooking'] = div.find("div", class_="m-srp-card__summary__info").text
+				if div.find("div", class_="m-srp-card__summary__title").text == "car parking":
+					dic['car_parking'] = div.find("div", class_="m-srp-card__summary__info").text
+			writer.writerow(dic)
