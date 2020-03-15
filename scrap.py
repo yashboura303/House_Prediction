b80c2b171e8cf58c864eb4dcea18d6ce5f9d8afa
yash <yashboura303@gmail.com>
2020-03-15 14:46:46 +0530

    added lat and long feature

--- scrap.py
+++ scrap.py
@@ -2,22 +2,52 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 l = []
+def get_lat(strr):
+	lat = strr.split(';')[1][16:].split(',')[0]
+	lat = lat[1:len(lat)-1]
+	return lat
+def get_long(strr):
+	long = strr.split(';')[1][16:].split(',')[1]
+	long = long[1:len(long)-1]
+	return long
 with open('data_set.csv', 'w', newline='') as csvfile:
-	fieldnames = [ 'carpet_area', 'bedrooms', 'bathrooms', 'car_parking', 'furnishing', 'transaction_type','floor', 'facing', 'overlooking', 'price']
+	fieldnames = [ 'carpet_area', 'bedrooms', 'bathrooms', 'car_parking', 'furnishing', 'transaction_type','floor', 'facing', 'overlooking', 'price', 'lat', 'long']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
-	for i in range(5):
+	for i in range(3):
 		source = requests.get(f'https://www.magicbricks.com/flats-in-mumbai-for-sale-pppfs/page-{i}').text
+		# source = requests.get('https://www.magicbricks.com/property-for-sale/residential-real-estate?proptype=Multistorey-Apartment,Builder-Floor-Apartment,Penthouse,Studio-Apartment&Locality=Borivali-East,Borivali-West&cityName=Mumbai').text
 		soup = BeautifulSoup(source, 'lxml')
 		for div in soup.find_all("div", class_='flex relative clearfix m-srp-card__container'):
 			dic ={}
 			if div.find("span", class_="m-srp-card__price"):
 				dic['price']=div.find("span", class_="m-srp-card__price").text
-			# else:
-			# 	# print("Call for price")
+			else:
+				continue
 			if div.find("span", class_="m-srp-card__title__bhk"):
 				dic['bedrooms']=div.find("span", class_="m-srp-card__title__bhk").text.split()[0]
+			if div.find("div", class_='m-srp-card__link m-srp-card__link--nearby'):
+				if get_lat(div.find("div", class_='m-srp-card__link m-srp-card__link--nearby').get('onclick')) == '0.0':
+					continue
+				else:
+					# print(get_lat(div.find("div", class_='m-srp-card__link m-srp-card__link--nearby').get('onclick')))
+					dic['lat'] = get_lat(div.find("div", class_='m-srp-card__link m-srp-card__link--nearby').get('onclick'))
+					dic['long'] = get_long(div.find("div", class_='m-srp-card__link m-srp-card__link--nearby').get('onclick'))
+			else:
+				continue
+					
+			dic["car_parking"] = 0
 			for div in (div.find_all("div", class_="m-srp-card__summary__item")):
+				if div.find("div", class_="m-srp-card__summary__title").text == "car parking":
+					main_string = div.find("div", class_="m-srp-card__summary__info").text.strip()
+					print(main_string)
+					if main_string:
+						if len(main_string) > 10:
+							dic['car_parking']=int(main_string.split(",")[0].split()[0]) + int(main_string.split(",")[1].split()[0])
+						elif len(main_string) <= 10 and len(main_string)>1:
+							dic["car_parking"] = int(main_string.split(",")[0].split()[0])
+					# else:
+					# 		dic["car_parking"] = 0
 				if div.find("div", class_="m-srp-card__summary__title").text == "bathroom":
 					dic['bathrooms'] = div.find("div", class_="m-srp-card__summary__info").text
 				if div.find("div", class_="m-srp-card__summary__title").text == "furnishing":
@@ -45,15 +75,7 @@ with open('data_set.csv', 'w', newline='') as csvfile:
 					dic['facing'] = div.find("div", class_="m-srp-card__summary__info").text
 				if div.find("div", class_="m-srp-card__summary__title").text == "overlooking":
 					dic['overlooking'] = div.find("div", class_="m-srp-card__summary__info").text
-				if div.find("div", class_="m-srp-card__summary__title").text == "car parking":
-					main_string = div.find("div", class_="m-srp-card__summary__info").text.strip()
-					if main_string:
-						if len(main_string) > 10:
-							dic['car_parking']=int(main_string.split(",")[0].split()[0]) + int(main_string.split(",")[1].split()[0])
-						elif len(main_string) <= 10 and len(main_string)>1:
-							dic["car_parking"] = int(main_string.split(",")[0].split()[0])
-					else:
-							dic["car_parking"] = 0
+				
 					# dic['car_parking'] = div.find("div", class_="m-srp-card__summary__info").text
 			writer.writerow(dic)
 
